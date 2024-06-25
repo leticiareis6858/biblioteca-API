039a43d16f86ef66bacad40cf8c9d6dcbda5cf51
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-06-25 20:59:01 -0300

    feat: adiciona a service de Usuario

---
+++ src/main/java/com/uninter/biblioteca/service/UsuarioService.java
@@ -0,0 +1,57 @@
+package com.uninter.biblioteca.service;
+
+import com.uninter.biblioteca.model.entity.Usuario;
+import com.uninter.biblioteca.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario criarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizarUsuario(Long id, Usuario usuario) {
+        Usuario usuarioExistente = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + id));
+
+        if (usuario.getCargo() != null) {
+            throw new IllegalArgumentException("Não é permitido mudar o cargo do usuário.");
+        }
+
+        if (usuario.getNome() != null) {
+            usuarioExistente.setNome(usuario.getNome());
+        }
+        if (usuario.getEmail() != null) {
+            usuarioExistente.setEmail(usuario.getEmail());
+        }
+        if (usuario.getSenha() != null) {
+            usuarioExistente.setSenha(usuario.getSenha());
+        }
+        if (usuario.getCargo() != null) {
+            usuarioExistente.setCargo(usuario.getCargo());
+        }
+
+        return usuarioRepository.save(usuarioExistente);
+    }
+
+
+    public void removerUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    public Usuario obterUsuarioPorId(Long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public List<Usuario> obterTodosUsuarios() {
+        return usuarioRepository.findAll();
+    }
+}
