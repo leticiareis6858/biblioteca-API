6f9a118926b57433594745924477655b0b4c15a0
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-06-25 17:17:39 -0300

    feat: adiciona o repositorio de usuario

---
+++ src/main/java/com/uninter/biblioteca/model/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.uninter.biblioteca.model.repository;
+
+import com.uninter.biblioteca.model.dao.AbstractDao;
+import com.uninter.biblioteca.model.dao.UsuarioDao;
+import com.uninter.biblioteca.model.entity.Usuario;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsuarioRepository extends AbstractDao<Usuario, Long> implements UsuarioDao {
+
+}
