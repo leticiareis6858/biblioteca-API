ee1bfe518bba71fe42098d6e2692e057ae532536
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-06-25 17:17:14 -0300

    feat: adiciona o repositorio de Livro

---
+++ src/main/java/com/uninter/biblioteca/model/repository/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.uninter.biblioteca.model.repository;
+
+import com.uninter.biblioteca.model.dao.AbstractDao;
+import com.uninter.biblioteca.model.dao.LivroDao;
+import com.uninter.biblioteca.model.entity.Livro;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LivroRepository extends AbstractDao<Livro, Long> implements LivroDao {
+
+}
