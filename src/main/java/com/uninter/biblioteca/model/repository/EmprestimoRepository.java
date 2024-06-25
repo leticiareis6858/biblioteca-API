57adfe3eb3f947ed7123b4af57afbbede122fc19
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-06-25 17:16:45 -0300

    feat: adiciona o repositorio de Emprestimo

---
+++ src/main/java/com/uninter/biblioteca/model/repository/EmprestimoRepository.java
@@ -0,0 +1,11 @@
+package com.uninter.biblioteca.model.repository;
+
+import com.uninter.biblioteca.model.dao.AbstractDao;
+import com.uninter.biblioteca.model.dao.EmprestimoDao;
+import com.uninter.biblioteca.model.entity.Emprestimo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmprestimoRepository extends AbstractDao<Emprestimo, Long> implements EmprestimoDao {
+
+}
