c9b2795623f8d6b982f963966e548ec46d668cc3
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-06-25 21:00:05 -0300

    feat: adiciona a service de Emprestimo

---
+++ src/main/java/com/uninter/biblioteca/service/EmprestimoService.java
@@ -0,0 +1,36 @@
+package com.uninter.biblioteca.service;
+
+import com.uninter.biblioteca.model.entity.Emprestimo;
+import com.uninter.biblioteca.model.repository.EmprestimoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmprestimoService {
+
+    @Autowired
+    private EmprestimoRepository emprestimoRepository;
+
+    public Emprestimo criarEmprestimo(Emprestimo emprestimo) {
+        return emprestimoRepository.save(emprestimo);
+    }
+
+    public Emprestimo atualizarEmprestimo(Emprestimo emprestimo) {
+        return emprestimoRepository.save(emprestimo);
+    }
+
+    public void removerEmprestimo(Long id) {
+        emprestimoRepository.deleteById(id);
+    }
+
+    public Emprestimo obterEmprestimoPorId(Long id) {
+        return emprestimoRepository.findById(id).orElse(null);
+    }
+
+    public List<Emprestimo> obterTodosEmprestimos() {
+        return emprestimoRepository.findAll();
+    }
+
+}
