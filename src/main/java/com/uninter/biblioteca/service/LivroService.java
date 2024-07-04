4cb0a6d6780b8967fbf760323359eac2858e8885
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-07-04 20:39:56 -0300

    fix: adiciona anotacao autowired faltante

--- src/main/java/com/uninter/biblioteca/service/LivroService.java
+++ src/main/java/com/uninter/biblioteca/service/LivroService.java
@@ -6,6 +6,7 @@ import com.uninter.biblioteca.model.entity.Livro;
 import static com.uninter.biblioteca.model.enumeration.Disponibilidade.DISPONIVEL;
 
 import com.uninter.biblioteca.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @Service
 public class LivroService {
 
+    @Autowired
     private LivroRepository livroRepository;
 
     // método para criar um livro
